3bd40bf6d9591023bf2d2000f501b4ccdc1842b4
LukaninaAD <lukaninaanastasia7@gmail.com>
2026-03-04 22:10:50 +0700

    хз

--- lab1/OOP_Fundamentals_library/IPayable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OOP_Fundamentals_library
-{
-    public interface IPayable
-    {
-        void ProcessSalary();
-        decimal CalculateBonus(int years, bool hasCertification);
-    }
-}
--- lab1/OOP_Fundamentals_library/IReport.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OOP_Fundamentals_library
-{
-    public interface IReport
-    {
-        void GenerateReport();
-    }
-}

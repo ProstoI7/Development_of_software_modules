e96174d9ea984f0efc908c0fcf383dd70788a7da
LukaninaAD <lukaninaanastasia7@gmail.com>
2026-05-04 12:14:41 +0700

    всё

---
+++ lab10/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using lab9.Services;
+using lab9.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace lab9
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // 1. Создаём коллекцию сервисов
+            var services = new ServiceCollection();
+
+            // 2. Регистрируем сервисы (Lifetime)
+            // DialogService — Singleton(один экземпляр на всё приложение), так как он не хранит состояние пользователя.
+            services.AddSingleton<IDialogService, DialogService>();
+
+            // 3. MainViewModel — Transient(новый экземпляр при каждом запросе) (при навигации нам будут нужны новые экземпляры)
+            services.AddTransient<MainViewModel>();
+
+            // 4. MainWindow  — Singleton с явной передачей DataContext через лямбда-выражение
+            // Окно создаётся один раз, DataContext получается из контейнера
+            services.AddSingleton<MainWindow>(sp =>
+            {
+                var window = new MainWindow();
+                window.DataContext = sp.GetRequiredService<MainViewModel>();
+                return window;
+            });
+
+            // 5. Создаём контейнер (ServiceProvider)
+            var serviceProvider = services.BuildServiceProvider();
+
+            // 6. Получаем главное окно из контейнера и запускаем приложение
+            var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ lab10/ViewModels/MainViewModel.cs
@@ -0,0 +1,103 @@
+﻿using lab9.Models;
+using lab9.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace lab9.ViewModels
+{
+    /// <summary>
+    /// Главная ViewModel приложения — посредник между Model (Contact) и View (MainWindow).
+    /// Содержит логику представления: коллекцию контактов, команды добавления/удаления.
+    /// </summary>
+    public class MainViewModel : ObservableObject
+    {
+        private readonly IDialogService _dialogService;
+        public ObservableCollection<Contact> Contacts { get; }
+
+        private string _name = string.Empty;
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+
+        private string _phone = string.Empty;
+        public string Phone
+        {
+            get => _phone;
+            set => Set(ref _phone, value);
+        }
+
+        private Contact? _selectedContact;
+        public Contact? SelectedContact
+        {
+            get => _selectedContact;
+            set => Set(ref _selectedContact, value);
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+        public MainViewModel(IDialogService dialogService)
+        {
+            _dialogService = dialogService ?? throw new System.ArgumentNullException(nameof(dialogService));
+            Contacts = new ObservableCollection<Contact>();
+
+            AddCommand = new RelayCommand(AddContact, CanAddContact);
+            DeleteCommand = new RelayCommand(DeleteContact, CanDeleteContact);
+        }
+
+        private void AddContact()
+        {
+            if (Contacts.Any(c => c.Phone == Phone))
+            {
+                _dialogService.ShowWarning("Контакт с таким номером телефона уже существует!");
+                return;
+            }
+
+            try
+            {
+                var newContact = new Contact(Name, Phone);
+
+                Contacts.Add(newContact);
+
+                _dialogService.ShowInfo($"Контакт {Name} успешно добавлен.");
+
+                Name = string.Empty;
+                Phone = string.Empty;
+            }
+            catch (ArgumentException ex)
+            {
+                _dialogService.ShowWarning(ex.Message, "Ошибка валидации");
+            }
+        }
+
+        private bool CanAddContact()
+        {
+            return !string.IsNullOrWhiteSpace(Name) && !string.IsNullOrWhiteSpace(Phone);
+        }
+
+        private void DeleteContact()
+        {
+            if (SelectedContact != null)
+            {
+                bool isConfirmed = _dialogService.ShowConfirmation($"Вы уверены, что хотите удалить контакт {SelectedContact.Name}?");
+                if (isConfirmed)
+                {
+                    Contacts.Remove(SelectedContact);
+                }
+            }
+        }
+
+        private bool CanDeleteContact()
+        {
+            return SelectedContact != null;
+        }
+    }
+}
